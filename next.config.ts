2939488d5eb73252b92ab314d43ff0d67d7bd252
Kartikeya Gupta <kartikeyagupta.bs23@bmsce.ac.in>
2025-10-13 13:18:41 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -23,11 +23,17 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
